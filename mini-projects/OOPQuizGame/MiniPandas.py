1bac16ce93d20909edfb00dff282d8dbf03d79a0
Shivam Singh <singhshivam11230@gmail.com>
2026-08-29 02:35:16 +0530

    code

--- mini-projects/OOPQuizGame/MiniPandas.py
+++ mini-projects/OOPQuizGame/MiniPandas.py
@@ -1,16 +1,43 @@
+class Series:
+
+    def __init__(self, data):
+        self.data = data
+
+    def __str__(self):
+        return str(self.data)
+
+    def mean(self):
+        return sum(self.data) / len(self.data)
+
+    def median(self):
+        sorted_data = sorted(self.data)
+        mid = len(sorted_data) // 2
+        if len(sorted_data) % 2 == 0:
+            return (sorted_data[mid-1] + sorted_data[mid]) / 2
+        else:
+            return sorted_data[mid]
+    def sum(self):
+        return sum(self.data)
+
+    def min(self):
+        return min(self.data)
+
+    def max(self):
+        return max(self.data)
+
 class Dataframe:
 
-    def __init__(self, data, columns):
+    def __init__(self, data, columns): 
         self.data = data
         self.columns = columns
 
-    def __getitem__(self, column_name):
+    def __getitem__(self, column_name): 
         if isinstance(column_name, str):
             column_no = self.columns.index(column_name)
             result = []
             for i in range(0, len(self.data)):
                 result.append(self.data[i][column_no])
-            return result
+            return Series(result)
 
         else:
             col_pst = []
@@ -23,7 +50,6 @@ class Dataframe:
                     selected_row.append(row[position])
                 result.append(selected_row)
             return result
-
     def head(self, n=5):
         print(self.data[:n])
 
@@ -43,9 +69,6 @@ data = [
 
 columns = ["Name", "Age", "Weight"]
 
-a = [12,13,14,1,15,16]
-
-b = len(a) // 2
-print(b)
-
-print(a[b],a[b-1])
+df = Dataframe(data, columns)
+print(df["Weight"].median())
+print(df["Weight"].min())
